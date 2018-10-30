312b97120566ffc673d9d8bbdeadb9eb7f9701fc
acidtib <acidtib@protonmail.com>
2018-10-30 18:26:04 -0400

    wip

---
+++ qa/pull-tester/tests_config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Copyright (c) 2013-2016 The WeyCoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+SRCDIR="/home/arubinofaux/Code/weycoin-project/Weycoin"
+BUILDDIR="/home/arubinofaux/Code/weycoin-project/Weycoin"
+EXEEXT=""
+
+# These will turn into comments if they were disabled when configuring.
+ENABLE_WALLET=1
+ENABLE_UTILS=1
+ENABLE_WEYCOIND=1
+ENABLE_ZMQ=1
